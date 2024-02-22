ce9c2c7fd52a0d2b8bd8de0bd6c68d88f5521cdd
zf <san.ferreiro92@gmail.com>
2024-02-21 22:20:48 -0300

    minor changes done

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,32 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+
+  const inputName = useRef();
+  const [playerName, setPlayerName] = useState('');
+  
+
+
+
+  function handleClick(){
+    setPlayerName(inputName.current.value);
+    //^^ Aca estamos al borde del paradigma declarativo de react
+    inputName.current.value = '';
+    //^^ Aca es estrictamente imperativo, evitar siempre que sea posible. Casos como este, safan
+    
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      {/**
+       *  {playerName ?? "unknown entity"}
+       *  ===
+       *  {playerName ? playerName : "unknown entity"}
+       */}
+      <h2>Welcome {playerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
